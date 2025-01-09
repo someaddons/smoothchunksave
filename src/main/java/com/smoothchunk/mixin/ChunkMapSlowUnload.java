f59e1f03a12fc87833c0bcfbc76af0b2f80f61a5
someaddons <someaddons@github.com>
2025-01-09 14:23:31 +0100

    3.9: Improved slow unloading feature

--- src/main/java/com/smoothchunk/mixin/ChunkMapSlowUnload.java
+++ src/main/java/com/smoothchunk/mixin/ChunkMapSlowUnload.java
@@ -3,13 +3,14 @@ package com.smoothchunk.mixin;
 import com.smoothchunk.SmoothchunkMod;
 import it.unimi.dsi.fastutil.longs.LongLinkedOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ChunkMap;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Queue;
+import java.util.function.BooleanSupplier;
 
 @Mixin(ChunkMap.class)
 public class ChunkMapSlowUnload
@@ -18,6 +19,9 @@ public class ChunkMapSlowUnload
     @Final
     @Mutable
     public LongSet toDrop = new LongLinkedOpenHashSet();
+
+    @Unique
+    private int scheduledToUnloadAmount = 0;
 
     @ModifyConstant(method = "processUnloads", constant = @Constant(intValue = 200))
     private int setLimit(final int constant)
@@ -29,5 +33,23 @@ public class ChunkMapSlowUnload
     private int dropUntil(final int constant)
     {
         return 20000;
+    }
+
+    @Inject(method = "scheduleUnload", at = @At("HEAD"))
+    private void onSchedule(final long p_140182_, final ChunkHolder p_140183_, final CallbackInfo ci)
+    {
+        scheduledToUnloadAmount++;
+    }
+
+    @Inject(method = "processUnloads", at = @At(value = "INVOKE", target = "Ljava/lang/Runnable;run()V"))
+    private void onRunUnload(final BooleanSupplier p_140354_, final CallbackInfo ci)
+    {
+        scheduledToUnloadAmount = Math.max(0, scheduledToUnloadAmount - 1);
+    }
+
+    @Redirect(method = "processUnloads", at = @At(value = "INVOKE", target = "Ljava/util/Queue;size()I"))
+    private int getSize(final Queue instance)
+    {
+        return scheduledToUnloadAmount;
     }
 }
